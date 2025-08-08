59a5fd669762756778926001d939a140b3c8a9b6
Francisco Ruvalcaba C <32653773+frantizek@users.noreply.github.com>
2025-08-08 12:58:05 -0600

    Handle illegal operations

--- Java/CalculatorConundrum.java
+++ Java/CalculatorConundrum.java
@@ -1,13 +1,24 @@
+import java.util.Objects;
+
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        if ("+".equals(operation)) {
+
+        if (Objects.isNull(operation)) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        } else if ("".equals(operation)){
+            throw new IllegalArgumentException("Operation cannot be empty");
+        } else if ("+".equals(operation)) {
             return "" + operand1 + " + " + operand2 + " = " + (operand1 + operand2);
         } else if ("*".equals(operation)) {
             return "" + operand1 + " * " + operand2 + " = " + (operand1 * operand2);
         } else if ("/".equals(operation)) {
-            return "" + operand1 + " / " + operand2 + " = " + (operand1 / operand2);
+            if (operand2 == 0) {
+                return "" + operand1 + " / " + operand2 + " = undefined";
+            } else {
+                return "" + operand1 + " / " + operand2 + " = " + (operand1 / operand2);
+            }
         } else {
-            return "";
+            throw new IllegalOperationException("Operation '" + operation + "' does not exist");
         }
     }
 }
