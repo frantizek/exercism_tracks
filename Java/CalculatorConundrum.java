0674211e6ced4ef1f55bef5d5f794fb5175d5ad7
Francisco Ruvalcaba C <32653773+frantizek@users.noreply.github.com>
2025-08-08 12:13:52 -0600

    Safe string comparison - Using .equals() method

--- Java/CalculatorConundrum.java
+++ Java/CalculatorConundrum.java
@@ -1,10 +1,10 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        if (operation == "+") {
+        if ("+".equals(operation)) {
             return "" + operand1 + " + " + operand2 + " = " + (operand1 + operand2);
-        } else if (operation == "*") {
+        } else if ("*".equals(operation)) {
             return "" + operand1 + " * " + operand2 + " = " + (operand1 * operand2);
-        } else if (operation == "/") {
+        } else if ("/".equals(operation)) {
             return "" + operand1 + " / " + operand2 + " = " + (operand1 / operand2);
         } else {
             return "";
